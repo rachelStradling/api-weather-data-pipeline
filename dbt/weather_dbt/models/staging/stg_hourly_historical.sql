67c70908338e2087567d9df786c8de44c1703923
Rachel <rachel@MacBook-Pro-de-Rachel.local>
2026-01-07 01:39:48 +0100

    Fixed dbt configuration and added first staging model

---
+++ dbt/weather_dbt/models/staging/stg_hourly_historical.sql
@@ -0,0 +1,19 @@
+{{ config(materialized='view') }}
+
+select
+  * except(is_daytime),
+  safe_cast(sunrise as datetime) as sunrise_dt,
+  safe_cast(sunset  as datetime) as sunset_dt,
+  datetime(timestamp) as hour_dt,
+  case
+    when safe_cast(sunrise as datetime) is null
+      or safe_cast(sunset  as datetime) is null
+      or timestamp is null
+    then null
+    when datetime(timestamp) >= safe_cast(sunrise as datetime)
+     and datetime(timestamp) <  safe_cast(sunset  as datetime)
+    then true
+    else false
+  end as is_daytime
+from {{ source('weather', 'hourly_historical') }}
+
