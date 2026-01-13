638398b284419fc3ec4a9405f9bb182fe840751a
Rachel <rachel@MacBook-Pro-de-Rachel.local>
2026-01-13 17:16:00 +0100

    Remove legacy local aggregation scripts

--- pipelines/load.py
+++
@@ -1,8 +0,0 @@
-from utils.logger import get_logger
-from utils.config import HOURLY_OUTPUT_CSV_PATH
-
-logger = get_logger(__name__)
-
-def load(df):
-    df.to_csv(HOURLY_OUTPUT_CSV_PATH, index=False)
-    logger.info(f"Loaded {len(df)} rows into {HOURLY_OUTPUT_CSV_PATH}")
